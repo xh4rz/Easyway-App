add12216723f0c1dedec8112e11e1d85cf28c7ee
xh4rz <xh4rz@hotmail.com>
2020-11-19 00:47:34 -0500

    Subiendo archivos - Harold

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import FormControl from 'react-bootstrap/FormControl';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Logo from './images/Logo.png';
+import './styles/Navbar.css';
+
+class MyNavbar extends React.Component {
+  render() {
+    return (
+      <>
+        {/* Navbar Principal */}
+        <Navbar className="Navbar__primary">
+
+          <Navbar.Brand href="#home">
+            <img className="Logo" src={Logo} alt="mi navbar" />
+          </Navbar.Brand>
+
+          <InputGroup className="Input-search">
+            <FormControl
+              placeholder="¿Qué estás buscando?"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+            <InputGroup.Append>
+              <Button className="Search" variant="dark">
+                <i className="fas fa-search"></i>
+              </Button>
+            </InputGroup.Append>
+          </InputGroup>
+
+          <Nav className="Nav__primary d-flex justify-content-end">
+            <Nav.Link href="#cuenta">
+              <div className="d-flex justify-content-center">
+                <i className="fas fa-user fa-2x mb-2"></i>
+              </div>
+              Mi Cuenta
+            </Nav.Link>
+
+            <Nav.Link href="#pedidos">
+              <div className="d-flex justify-content-center">
+                <i className="fas fa-clipboard-list fa-2x mb-2"></i>
+              </div>
+              <span>Mis Pedidos</span>
+            </Nav.Link>
+            <Nav.Link href="#carrito">
+              <div className="d-flex justify-content-center">
+                <i className="fas fa-shopping-cart fa-2x mb-2"></i>
+              </div>
+              <span>Mi Carrito</span>
+            </Nav.Link>
+          </Nav>
+
+        </Navbar>
+
+        {/* Navbar Secundario */}
+        <Navbar collapseOnSelect expand="lg" className="Navbar__secondary">
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+          <Navbar.Collapse id="responsive-navbar-nav">
+
+            <Nav className="prueba">
+
+              <NavDropdown title="Categorias" id="collasible-nav-dropdown">
+
+                <NavDropdown.Item href="#action/3.1">
+                  Hogar y Bienestar
+                </NavDropdown.Item>
+
+                <NavDropdown.Item href="#action/3.2">
+                  Tecnología
+                </NavDropdown.Item>
+
+                <NavDropdown.Item href="#action/3.3">
+                  Alimentos
+                </NavDropdown.Item>
+
+                <NavDropdown.Item href="#action/3.4">
+                  Salud
+                </NavDropdown.Item>
+
+                <NavDropdown.Item href="#action/3.5">
+                  otros
+                </NavDropdown.Item>
+
+              </NavDropdown>
+
+              <Nav.Link href="#features">
+                <i className="fas fa-home mr-2"></i>
+                   Inicio
+              </Nav.Link>
+
+              <Nav.Link href="#pricing">
+                <i className="fas fa-tags mr-2"></i>
+                    Ofertas
+                </Nav.Link>
+
+              <Nav.Link href="#pricing">
+                <i className="fas fa-map-marker-alt mr-2"></i>
+                    Tiendas
+              </Nav.Link>
+
+              <Nav.Link href="#pricing">
+                <i className="fas fa-store mr-2"></i>
+                    Conócenos
+              </Nav.Link>
+
+              <Nav.Link href="#pricing">
+                <i className="fas fa-handshake mr-2"></i>
+                     Servicios
+              </Nav.Link>
+
+            </Nav>
+
+          </Navbar.Collapse>
+
+        </Navbar>
+      </>
+    );
+  }
+}
+
+export default MyNavbar;
